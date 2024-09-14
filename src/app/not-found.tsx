9ec6720e492074023d8cb4b8af66bdfe001c768c
johnasblasco <johnaslblasco@gmail.com>
2024-09-14 23:35:24 +0800

    what if

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notFound = () => {
+      return (
+            <div>
+                  <h1 className='m-auto'>Page not found nigga</h1>
+            </div>
+      )
+}
+
+export default notFound
